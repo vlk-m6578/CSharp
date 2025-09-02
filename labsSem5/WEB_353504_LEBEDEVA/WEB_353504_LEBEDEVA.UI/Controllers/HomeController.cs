629143d3af8fed1f6f9733aca8bfd62062fc7843
Milana L <lebedeva310@icloud.com>
2025-09-03 01:48:58 +0300

    lab1

---
+++ labsSem5/WEB_353504_LEBEDEVA/WEB_353504_LEBEDEVA.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_353504_LEBEDEVA.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
